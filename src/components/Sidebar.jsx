c78e0c3f73aa21882772aa0b5f34823ab45130a2
Arpit Indurkar <arpitindurkar48@gmail.com>
2023-06-09 16:59:00 +0530

    Videos are Rendering into the feed

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import { Stack } from "@mui/material";
 
-const Sidebar = () => {
-  return <div>Sidebar</div>;
-};
+import { categories } from "../utils/constants";
+import { Category } from "@mui/icons-material";
+
+const Sidebar = ({ selectedCategory, setselectedCategory }) => (
+  <Stack
+    direction="row"
+    sx={{
+      overflow: "auto",
+      height: { sx: "auto", md: "95%" },
+      flexDirection: { md: "column" },
+    }}
+  >
+    {categories.map((Category) => (
+      <button
+        className="category-btn"
+        onClick={() => setselectedCategory(Category.name)}
+        style={{
+          background: Category.name === selectedCategory && "#FC1503",
+          color: "white",
+        }}
+        key={Category.name}
+      >
+        <span
+          style={{
+            color: Category.name === selectedCategory ? "white" : "red",
+            marginRight: "15px",
+          }}
+        >
+          {Category.icon}
+        </span>
+        <span
+          style={{ opacity: Category.name === selectedCategory ? "1" : "0.8" }}
+        >
+          {Category.name}
+        </span>
+      </button>
+    ))}
+  </Stack>
+);
 
 export default Sidebar;
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import { Stack, Box } from "@mui/material";
+import { VideoCard, ChannelCard } from "./";
 
-const Videos = () => {
-  return <div>Videos</div>;
+const Videos = ({ videos }) => {
+  return (
+    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+      {videos.map((items, idx) => (
+        <Box key={idx}>
+          {items.id.videoId && <VideoCard video={items} />}
+          {items.id.channelId && <ChannelCard channelDetail={items} />}
+        </Box>
+      ))}
+    </Stack>
+  );
 };
 
 export default Videos;
